50e9b7bbd06aebf1d38d639b9e18756e84191e08
i-am-expert <onlyforgithub2708@gmail.com>
2020-05-16 15:35:31 +0530

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+// All Authors route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query 
+        });
+    } catch {
+        res.redirect('/')
+    }
+});
+
+// New author route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
+});
+
+// The above route is just to display
+// Route to actually create new author
+
+// Create author route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect('authors')
+    } catch (error) {
+        let locals = {
+            author: author,
+            errorMessage: 'Error creating author'
+        }
+        res.render('authors/new', locals);
+    }
+});
+
+module.exports = router
